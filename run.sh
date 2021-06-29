50086c8368a55bae9ad8d1c1cd5c7dc138b1340f
eellena <ezequielellena0003@gmail.com>
2021-06-28 22:06:22 -0300

    initial commit

---
+++ run.sh
@@ -0,0 +1,149 @@
+
+#!/bin/bash
+
+ps -aux | grep python3
+
+
+killall -KILL python3
+python3 RasaTelegram.py > /dev/null 2>&1 &
+python3 ScraperWordpress.py > /dev/null 2>&1 &
+python3 ScraperWorrdpressASC.py > /dev/null 2>&1 &
+python3 NuevoScrapperWordpress.py > /dev/null 2>&1 &
+python3 NuevoScrapperWordPressASC.py > /dev/null 2>&1 &
+
+python3 EnviaTelegram.py 01 > /dev/null 2>&1 &
+python3 EnviaTelegramArgentina.py 50 > /dev/null 2>&1 &
+python3 EnviaTelegramArgentina.py 81 > /dev/null 2>&1 &
+python3 EnviaTelegramArgentina.py 45 > /dev/null 2>&1 &
+python3 EnviaTelegramArgentina.py 62 > /dev/null 2>&1 &
+python3 EnviaTelegramArgentina.py 41 > /dev/null 2>&1 &
+python3 EnviaTelegramArgentina.py 01 > /dev/null 2>&1 &
+python3 EnviaTelegramArgentina.py 59 > /dev/null 2>&1 &
+python3 EnviaTelegramArgentina.py 32 > /dev/null 2>&1 &
+python3 EnviaTelegramArgentina.py 57 > /dev/null 2>&1 &
+python3 EnviaTelegramArgentina.py 87 > /dev/null 2>&1 &
+python3 EnviaTelegramArgentina.py 34 > /dev/null 2>&1 &
+
+
+python3 ScrapperDominicanaRss.py  1 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  2 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  3 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  4 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  5 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  6 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  7 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  8 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  9 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  10 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  11 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  12 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  13 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  14 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  15 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  16 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  17 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  18 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  19 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  20 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  21 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  22 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  23 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  24 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  25 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  26 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  27 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  28 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  29 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  30 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  31 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  32 > /dev/null 2>&1 &
+python3 ScrapperDominicanaRss.py  33 > /dev/null 2>&1 &
+
+python3 ScapperDominicana.py  1 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  2 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  3 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  4 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  5 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  6 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  7 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  8 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  9 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  10 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  11 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  12 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  13 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  14 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  15 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  16 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  17 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  18 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  19 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  20 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  21 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  22 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  23 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  24 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  25 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  26 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  27 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  28 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  29 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  30 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  31 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  32 > /dev/null 2>&1 &
+python3 ScapperDominicana.py  33 > /dev/null 2>&1 &
+
+
+
+python3 ScrapperArgentina.py  1 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  2 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  3 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  4 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  5 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  6 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  7 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  8 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  9 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  10 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  11 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  12 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  13 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  14 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  15 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  16 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  17 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  18 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  19 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  20 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  21 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  22 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  23 > /dev/null 2>&1 &
+python3 ScrapperArgentina.py  24 > /dev/null 2>&1 &
+
+
+
+python3 NuevoScrapperRss.py 1 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 2 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 3 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 4 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 5 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 6 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 7 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 8 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 9 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 10 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 11 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 12 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 13 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 14 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 15 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 16 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 17 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 18 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 19 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 20 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 21 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 22 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 23 > /dev/null 2>&1 &
+python3 NuevoScrapperRss.py 24 > /dev/null 2>&1 &
+
